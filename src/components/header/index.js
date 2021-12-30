9cad41eff9ad9fef62e77d23ef2c33ba9fb7d4f8
ahmadm712 <ahmadmuji2808@gmail.com>
2021-12-31 01:17:24 +0700

    update pemakean React Bootsrap

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,39 +1,38 @@
 import React from "react";
 
+
 function Header() {
   return (
-      <header id="header" class="fixed-top">
-        <div class="container d-flex align-items-center justify-content-between">
-          <h1 class="logo">
-            <a href="index.html">YIA</a>
-          </h1>
-
-          {/* <a href="index.html" class="logo">
+    <header id="header" class="fixed-top">
+      <div class="container d-flex align-items-center justify-content-between">
+        <h1 class="logo">
+          <a href="index.html">YIA</a>
+        </h1>
+        
+        {/* <a href="index.html" class="logo">
         <img src="assets/img/logo.png" alt="" class="img-fluid"/></a> */}
+        <nav id="navbar" class="navbar">
+          <ul>
+            <li>
+              <a class="nav-link scrollto active" href="#hero">
+                Home
+              </a>
+            </li>
+            <li>
+              <a class="nav-link scrollto" href="#about">
+                Aspiration
+              </a>
+            </li>
 
-          <nav id="navbar" class="navbar">
-            <ul>
-              <li>
-                <a class="nav-link scrollto active" href="#hero">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a class="nav-link scrollto" href="#about">
-                  Aspiration
-                </a>
-              </li>
-
-              <li class="dropdown">
-                <a href="#">
-                  <span>Article Category</span>{" "}
-                  <i class="bi bi-chevron-down"></i>
-                </a>
-                <ul>
-                  <li>
-                    <a href="#">Human Right</a>
-                  </li>
-                  {/* <li class="dropdown">
+            <li class="dropdown">
+              <a href="#">
+                <span>Article Category</span> <i class="bi bi-chevron-down"></i>
+              </a>
+              <ul>
+                <li>
+                  <a href="#">Human Right</a>
+                </li>
+                {/* <li class="dropdown">
                     <a href="#">
                       <span>Deep Drop Down</span>{" "}
                       <i class="bi bi-chevron-right"></i>
@@ -56,39 +55,37 @@ function Header() {
                       </li>
                     </ul>
                   </li> */}
-                  <li>
-                    <a href="#">Mental Health</a>
-                  </li>
-                  <li>
-                    <a href="#">Jobs</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a class="nav-link scrollto" href="#about">
-                  About
-                </a>
-              </li>
-              <form class="d-flex ms-4">
-                <input
-                  class="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
                 <li>
+                  <a href="#">Mental Health</a>
+                </li>
+                <li>
+                  <a href="#">Jobs</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a class="nav-link scrollto" href="#about">
+                About
+              </a>
+            </li>
+            <form class="d-flex ms-4">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <li>
                 <a class="getstarted scrollto" href="#about">
                   Login
                 </a>
               </li>
-              </form>
-              
-            </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
-          </nav>
-        </div>
-      </header>
-    
+            </form>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+      </div>
+    </header>
   );
 }
 
--- src/components/list/list_aspiraion/index.js
+++ src/components/list/list_aspiraion/index.js
@@ -1,19 +1,23 @@
 import React from "react";
 import CardAspirasi from "../../card/card_aspirasi";
 import Aspiration from "../../../data/json/aspiration.json"
+import {Row,Col} from 'react-bootstrap'
 
 export default function ListAspiration() {
   return (
-    <div>
+    <Row>
       <h1>Some Popular Aspiration</h1>
       {
         Aspiration.map((value)=>{
           return (
-            <CardAspirasi key={value.category_id} />
+            <Col>
+            <CardAspirasi key={value.category_id} {...value}/>
+              
+            </Col>
 
           );
         })
       }
-    </div>
+    </Row>
   );
 }
