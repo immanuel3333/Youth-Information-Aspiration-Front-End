a64924549b621310767925281f5d1d59c1e76477
Rafly Ramadhani Putra <raflyramadhaniputra28@gmail.com>
2021-12-31 18:16:08 +0700

    Update progress login static

--- src/pages/loginPage/index.js
+++ src/pages/loginPage/index.js
@@ -1,57 +1,75 @@
 import React from "react";
+import { Form, Row, Button, Col, Container } from "react-bootstrap";
 
+// import YIABackground from "./assets/image/YIA-Background.png";
 
 function LoginPage() {
+	// const sectionStyle = {
+  //   backgroundImage: `url(${"https://i.ibb.co/kmSGNGK/group-asia-young-creative-people-smart-casual-wear-smiling-arms-crossed-creative-office-workplace-di.png"})`
+  // } 
     
     return (
-        <div>
-		<body className="img js-fullheight" style={"background-image: url(https://i.ibb.co/d0MtG5S/4b78da0ac3c63fa6901053af55a5d0f1.jpg);"}>
-	<section className="ftco-section">
-		<div class="container">
-			<div className="row justify-content-center">
-				<div className="col-md-6 text-center mb-5">
-					<h2 className="heading-section">Login #10</h2>
-				</div>
-			</div>
-			<div className="row justify-content-center">
-				<div className="col-md-6 col-lg-4">
-					<div className="login-wrap p-0">
-		      	<h3 className="mb-4 text-center">Have an account?</h3>
-		      	<form action="#" className="signin-form">
-		      		<div className="form-group">
-		      			<input type="text" className="form-control" placeholder="Username" required></input>
-		      		</div>
-	            <div className="form-group">
-	              <input id="password-field" type="password" class="form-control" placeholder="Password" required></input>
-	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-	            </div>
-	            <div className="form-group">
-	            	<button type="submit" className="form-control btn btn-primary submit px-3">Sign In</button>
-	            </div>
-	            <div className="form-group d-md-flex">
-	            	<div className="w-50">
-		            	<label className="checkbox-wrap checkbox-primary">Remember Me
-									  <input type="checkbox" checked></input>
-									  <span class="checkmark"></span>
-									</label>
-								</div>
-								<div className="w-50 text-md-right">
-									<a href="#" style="color: #fff">Forgot Password</a>
-								</div>
-	            </div>
-	          </form>
-	          <p className="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
-	          <div class="social d-flex text-center">
-	          	<a href="#" className="px-2 py-2 mr-md-1 rounded"><span className="ion-logo-facebook mr-2"></span> Facebook</a>
-	          	<a href="#" className="px-2 py-2 ml-md-1 rounded"><span className="ion-logo-twitter mr-2"></span> Twitter</a>
-	          </div>
-		      </div>
-				</div>
-			</div>
-		</div>
-	</section>
-    </body>
-	</div>
+    
+  <Container className="p-0 background-image" fluid={true}>
+   
+
+  
+		{/* <Container className="color-overlay d-flex justify-content-center align-items-center"> */}
+
+   
+    
+      
+    <div className='color-overlay'>
+    
+    <div className='color-overlay d-flex justify-content-center align-items-center'>
+
+    <Form className="form-rounded p-4">
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+
+  <h2 className="text-center sign-in-text">Welcome!</h2>
+
+  <h7 className="text-center">Please Login First, to Use Our Service</h7>
+
+<br>
+</br>
+<br>
+</br>
+
+    <Form.Label className="text-email">Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted text-remind">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className="text-password">Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check className="text-remember" type="checkbox" label="Remember Me" />
+      <br>
+      </br>
+    <h7 className="forgot-password">Forgot your Password?</h7>
+  </Form.Group>
+  <div className="d-grid gap-2">
+  <Button variant="primary" size="md">
+    Sign In
+  </Button>
+  <Button variant="success" size="md">
+    Create an Account
+  </Button>
+</div>
+
+</Form>
+    </div>
+    </div>
+    
+  </Container>
+// </Container>
+
+
+
     )
 }
 
