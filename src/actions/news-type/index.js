85276e3ae1b2b85dbf63b5b8cec3f4be1ce1ab32
ahmadm712 <ahmadmuji2808@gmail.com>
2022-01-09 16:31:33 +0700

    menambahkan fitur news_by id pakai redux

--- src/actions/news-type/index.js
+++ src/actions/news-type/index.js
@@ -1,2 +1,3 @@
 export const GET_NEWS = "GET_NEWS";
+export const GET_NEWS_BY_ID = "GET_NEWS_BY_ID";
 export const NEWS_ERROR = "NEWS_ERROR";
--- src/pages/detail_news/index.js
+++ src/pages/detail_news/index.js
@@ -1,90 +1,120 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
-import { Row, Container, Col, Badge, Tabs, Tab, Sonnet } from "react-bootstrap";
+import {
+  Row,
+  Container,
+  Col,
+  Badge,
+  Tabs,
+  Tab,
+  Sonnet,
+  Spinner,
+} from "react-bootstrap";
 import CardNewsRecomend from "../../components/card/card_news_recomend";
 import { useParams } from "react-router-dom";
 import newsJson from "../../data/json/news.json";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getNewsById } from "../../actions/news-action";
 
 export default function DetailNewsPage() {
+  const dispatch = useDispatch();
+
   const newsData = useSelector((state) => state.news);
+  const { id } = useParams();
+
   const { news } = newsData;
 
-  const { id } = useParams();
-  console.log(id);
+  useEffect(() => {
+    dispatch(getNewsById(id));
+  }, [dispatch]);
 
-  const News = newsJson.find((res) => res._id === Number(id));
-
-  console.log(News);
   // console.log(news);
-  return (
-    <div>
-      <Header />
-      <br />
-      <br />
-      <br />
-      <Container>
-        <Row className="align-items-start">
-          <Col className="col-lg-8 m-15px-tb">
-            <div className="article">
-              <div className="article-title">
-                <h2>{News.news_title}</h2>
-                <div className="py-1 d-flex">
-                  <Badge bg="secondary" className="me-2">
-                    HAM
-                    {/* {News.category_id.find((e) => e.$oid)} */}
-                  </Badge>
-                  <div className="me-2">Published {News.created_at.$date}</div>
-                  <div className="me-2">Source : {News.news_source}</div>
+
+  // console.log(news.data.News);
+  if (news != null || news.data != undefined) {
+    return (
+      <div>
+        <Header />
+        <br />
+        <br />
+        <br />
+        <Container>
+          <Row className="align-items-start">
+            <Col className="col-lg-8 m-15px-tb">
+              <div className="article">
+                <div className="article-title">
+                  <h2>{news.news_title}</h2>
+                  <div className="py-1 d-flex">
+                    <Badge bg="secondary" className="me-2">
+                      HAM
+                      {/* {News.category_id.find((e) => e.$oid)} */}
+                    </Badge>
+                    <div className="me-2">
+                      Published{" "}
+                      {news.created_at != null || news.created_at != undefined
+                        ? news.created_at.$date
+                        : "kosong"}
+                    </div>
+                    <div className="me-2">Source : {news.news_source}</div>
+                  </div>
+                </div>
+
+                <img
+                  src={news.news_image}
+                  style={{
+                    borderRadius: "8px",
+                    height: "400px",
+                    width: "100%",
+                  }}
+                  className="img-fluid"
+                />
+
+                <div className="article-content mt-4">
+                  <p>{news.news_description}</p>
+
+                  <blockquote></blockquote>
                 </div>
               </div>
-
-              <img
-                src={News.news_image}
-                style={{ borderRadius: "8px", height: "400px", width: "100%" }}
-                className="img-fluid"
-              />
-
-              <div className="article-content mt-4">
-                <p>{News.news_description}</p>
-
-                <blockquote></blockquote>
+            </Col>
+            <Col>
+              <div className="article mx-auto justify-content-between">
+                <i className="me-4 fab fa-instagram fa-2x" />
+                <i className="me-4 fab fa-facebook fa-2x" />
+                <i className="me-4 fab fa-linkedin fa-2x" />
+                <i className="me-4 fas fa-share-alt fa-2x" />
               </div>
-            </div>
-          </Col>
-          <Col>
-            <div className="article mx-auto justify-content-between">
-              <i className="me-4 fab fa-instagram fa-2x" />
-              <i className="me-4 fab fa-facebook fa-2x" />
-              <i className="me-4 fab fa-linkedin fa-2x" />
-              <i className="me-4 fas fa-share-alt fa-2x" />
-            </div>
 
-            <div className="article" style={{ marginTop: "100px" }}>
-              <Tabs
-                defaultActiveKey="popular"
-                id="uncontrolled-tab-example"
-                transition={false}
-                className="justify-content-between"
-              >
-                <Tab eventKey="popular" title="Popular">
-                  <CardNewsRecomend />
-                  <hr />
-                  <CardNewsRecomend />
-                </Tab>
-                <Tab eventKey="recomended" title="Recomended">
-                  <CardNewsRecomend />
-                  <hr />
-                  <CardNewsRecomend />
-                </Tab>
-              </Tabs>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Footer />
-    </div>
-  );
+              <div className="article" style={{ marginTop: "100px" }}>
+                <Tabs
+                  defaultActiveKey="popular"
+                  id="uncontrolled-tab-example"
+                  transition={false}
+                  className="justify-content-between"
+                >
+                  <Tab eventKey="popular" title="Popular">
+                    <CardNewsRecomend />
+                    <hr />
+                    <CardNewsRecomend />
+                  </Tab>
+                  <Tab eventKey="recomended" title="Recomended">
+                    <CardNewsRecomend />
+                    <hr />
+                    <CardNewsRecomend />
+                  </Tab>
+                </Tabs>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+
+        <Footer />
+      </div>
+    );
+  } else {
+    <Spinner animation="border" role="status" className="mx-auto">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>;
+  }
 }
